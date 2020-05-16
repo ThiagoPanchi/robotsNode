16ed00e8fc808168328a13a59654f9a36dc50074
Thiago Panchiniak <panchiniak@gmail.com>
2020-05-16 15:55:57 -0300

    add download image robot

--- robots/image.js
+++ robots/image.js
@@ -1,3 +1,4 @@
+const imageDownloader = require('image-downloader');
 const google = require('googleapis').google;
 const customSearch = google.customsearch('v1');
 const state = require('./state.js');
@@ -8,6 +9,8 @@ async function robot(){
   const content = state.load();
   
   await fetchImagesOfAllSentences(content);
+  await downloadAllImages(content);
+
   state.save(content);
 
   async function fetchImagesOfAllSentences(content) {
@@ -26,20 +29,46 @@ async function robot(){
       searchType: 'image',
       num: 2
     });
-    // ### TO DO - Correct error when query dont have image response.
-    //console.dir(response, {depth: null} );
-    //if(!response.data.items.link){
-    //  const empty = "Search Term not found any results..."
-    //  return empty;
-    //}
+    
+    // Return a empty string when no found any results with the query.
+    if(!response.data.items){
+      const empty = " ";
+      return empty;
+    }
     const imagesUrl = response.data.items.map((item) => {
       return item.link;
     });
     
     return imagesUrl;
   }
-  
+  async function downloadAllImages(content){
+    content.downloadedImages = [];
 
+    for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++){
+      const images = content.sentences[sentenceIndex].images;
+
+      for (let imageIndex = 0; imageIndex < images.length; imageIndex++){
+        const imageUrl = images[imageIndex];
+        try {
+          if(content.downloadedImages.includes(imageUrl)){
+            throw new Error('Imagem já foi baixada...');
+          }
+          await downloadAndSave(imageUrl,`${sentenceIndex}-original.png`)
+          content.downloadedImages.push(imageUrl)
+          console.log(`>[${sentenceIndex}][${imageIndex}] Baixou com sucesso: ${imageUrl}`);
+          break;
+        } catch(error){
+          console.log(`> Erro ao baixar(${imageUrl}): ${error}`);
+        }
+      }
+    }
+  }
+  async function downloadAndSave(url, fileName){
+    return imageDownloader.image({
+      url, url,
+      dest:`./content/${fileName}`
+    })
+  }
 }
 
 module.exports = robot;
